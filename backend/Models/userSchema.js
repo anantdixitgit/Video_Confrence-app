af2be6c4dd29431821dec799aad8980432fc32a2
anant_dixit_13 <anantdixit100@gmail.com>
2026-02-17 17:26:26 +0530

    hash round updated

--- backend/Models/userSchema.js
+++ backend/Models/userSchema.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
   try {
-    this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 6);
   } catch (err) {
     throw err;
   }
